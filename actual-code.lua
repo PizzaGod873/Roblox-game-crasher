22493596cd7cc99e25db599c00226ad6136e754a
Andrew <150487080+PizzaGod873@users.noreply.github.com>
2024-10-19 23:34:43 -0500

    Create actual-code.lua

---
+++ actual-code.lua
@@ -0,0 +1,6 @@
+while true do
+    local a = Instance.new("Part")
+    a.Parent = workspace
+    a.Anchored = false
+    a.CFrame = CFrame.new(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.random(1, 2147483647))))))))))))))))))))))),math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.random(1, 2147483647))))))))))))))))))))))),math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.sqrt(math.random(1, 2147483647))))))))))))))))))))))))
+end
